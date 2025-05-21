7acc857a9438b085e35c4e73bbe1a47fc4cc241f
yitzchak shaish <y350380350@gmail.com>
2025-05-21 16:13:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstStrike;
+
+namespace Solid_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Terrorist moohamad = new Terrorist("moohamad", 3,[ "coll", "sakin"],true);
+            //Console.WriteLine(moohamad);
+        }
+    }
+}
