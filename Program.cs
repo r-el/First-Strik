18335146c6da17edd969d6223b1010fa71bbbeea
Ariel <arieltanami122@gmail.com>
2025-05-21 16:46:44 +0300

    Moving the StrikeUnits folder into the Entities folder

--- Program.cs
+++ Program.cs
@@ -1,19 +1,18 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
-// using System.Threading.Tasks;
-// using FirstStrike;
-// using FirstStrike.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstStrike;
+using FirstStrike.Entities.StrikeUnits;
 
+// ---------- בדיקת יחידות תקיפה ---------- //
+F16FighterJet f16 = new F16FighterJet();
+Console.WriteLine($"Name: {f16.Name}");
+Console.WriteLine($"Ammo: {f16.Ammo}");
+Console.WriteLine($"Fuel: {f16.Fuel}");
+Console.WriteLine($"Effective Against: {string.Join(", ", f16.EffectiveAgainst)}");
 
-// Console.WriteLine("Hello, World!");
-
-// F16FighterJet f16 = new F16FighterJet();
-// Console.WriteLine($"Name: {f16.Name}");
-// Console.WriteLine($"Ammo: {f16.Ammo}");
-// Console.WriteLine($"Fuel: {f16.Fuel}");
-// Console.WriteLine($"Effective Against: {string.Join(", ", f16.EffectiveAgainst)}");
-
-//Terrorist moohamad = new Terrorist("moohamad", 3,[ "coll", "sakin"],true);
-//Console.WriteLine(moohamad);
+// ---------- בדיקת אנשים ---------- //
+Terrorist moohamad = new Terrorist("moohamad", 3,[ "coll", "sakin"],true);
+Console.WriteLine(moohamad);
