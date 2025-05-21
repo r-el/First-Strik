d76675d660b62a6125423b9e545ffe0f17d19b41
Ariel <arieltanami122@gmail.com>
2025-05-21 16:29:45 +0300

    Add F16FighterJet class implementing IStrikeUnit with properties

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FirstStrike.Entities;
+
 Console.WriteLine("Hello, World!");
+
+F16FighterJet f16 = new F16FighterJet();
+Console.WriteLine($"Name: {f16.Name}");
+Console.WriteLine($"Ammo: {f16.Ammo}");
+Console.WriteLine($"Fuel: {f16.Fuel}");
+Console.WriteLine($"Effective Against: {string.Join(", ", f16.EffectiveAgainst)}");
+
