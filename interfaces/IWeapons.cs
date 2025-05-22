b0dce8a72a381c46d933c01328ace561a4deadf0
yitzchak shaish <y350380350@gmail.com>
2025-05-22 16:50:17 +0300

    Added a interfaces of Hamas's types of weapons.

---
+++ interfaces/IWeapons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStrike.interfaces
+{
+    public interface IWeapons
+    {
+        string Name { get; }
+        int SeverityLevel { get; }
+
+    }
+}
