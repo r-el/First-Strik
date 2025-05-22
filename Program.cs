40d426552a7582a7f3d113f4882b0742c1d19724
yitzchak shaish <y350380350@gmail.com>
2025-05-22 15:46:55 +0300

    Fixed the program file with examples.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FirstStrike;
+using FirstStrike.Entities.Aman;
 using FirstStrike.Entities.StrikeUnits;
 
 // ---------- בדיקת יחידות תקיפה ---------- //
@@ -16,3 +17,17 @@ Console.WriteLine($"Effective Against: {string.Join(", ", f16.EffectiveAgainst)}
 // ---------- בדיקת אנשים ---------- //
 Terrorist moohamad = new Terrorist("moohamad", 3,[ "coll", "sakin"],true);
 Console.WriteLine(moohamad);
+// ---------- בדיקת אנשים ---------- //
+List<string> weapons = new List<string> { "coll", "sakin" };
+Terrorist moohamad2 = new Terrorist("moohamad", 3, weapons, true);
+Console.WriteLine(moohamad);
+
+Location locationMoohamad = new Location("home");
+
+IntelligenceMessages messag1 = new IntelligenceMessages(moohamad, locationMoohamad, DateTime.Now
+);
+Console.WriteLine(messag1);
+
+Aman aman = new();
+aman.AddIntelligenceMessages(messag1);
+Console.WriteLine(aman);
