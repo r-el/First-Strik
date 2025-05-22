24c39eb3e5f47096a62f5ae67b7713bd1a601337
yitzchak shaish <y350380350@gmail.com>
2025-05-22 12:42:31 +0300

    Added file Aman

---
+++ Entities/Aman/Aman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStrike.Entities.Aman
+{
+    internal class Aman
+    {
+
+    }
+}
